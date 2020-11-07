6011345db4c9c816e957345f25f0048975b63eca
msaaddev <mrsaadirfan@gmail.com>
2020-11-07 19:34:43 +0500

    📦 NEW: summarize component

---
+++ frontend/src/components/Summarize.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import '../styles/summarize.css';
+
+const Summarize = () => {
+    return (
+        <div className='container-smrz' id='summarize-area'>
+            <div className='sub-container-smrz'>
+                <div className='main-content-smrz'></div>
+                <div className='btn-smrz'></div>
+                <div className='summarize-content-smrz'></div>
+            </div>
+        </div>
+    );
+};
+
+export default Summarize;
