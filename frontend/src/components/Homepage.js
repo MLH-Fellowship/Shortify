e24255cd1383d6afb5cd490ca04ac3261129da6f
msaaddev <mrsaadirfan@gmail.com>
2020-11-07 19:34:25 +0500

    📦 NEW: scrollToSummarize function

--- frontend/src/components/Homepage.js
+++ frontend/src/components/Homepage.js
@@ -1,12 +1,29 @@
 import React from 'react';
 
+// libraries
+import scrollIntoView from 'scroll-into-view-if-needed';
+
 // components
 import FrontUI from './FrontUI';
+import Summarize from './Summarize';
+
+/**
+ *
+ * scroll to the summarize area
+ * @param {node} - id of the summarize area
+ */
+const scrollToSummarize = node => {
+    scrollIntoView(node, {
+        behavior: 'smooth',
+        scrollMode: 'if-needed',
+    });
+};
 
 const Homepage = () => {
     return (
         <div className='wrapper-hmpg'>
-            <FrontUI />
+            <FrontUI scrollToSummarize={scrollToSummarize} />
+            <Summarize />
         </div>
     );
 };
