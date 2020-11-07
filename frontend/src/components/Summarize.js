51c848cc3e889167aa246bba840280600358ffb0
msaaddev <mrsaadirfan@gmail.com>
2020-11-08 01:43:05 +0500

    👌 IMPROVE: text area and button

--- frontend/src/components/Summarize.js
+++ frontend/src/components/Summarize.js
@@ -5,9 +5,17 @@ const Summarize = () => {
     return (
         <div className='container-smrz' id='summarize-area'>
             <div className='sub-container-smrz'>
-                <div className='main-content-smrz'></div>
-                <div className='btn-smrz'></div>
-                <div className='summarize-content-smrz'></div>
+                <div className='main-content-smrz'>
+                    <h1>Your Content</h1>
+                    <textarea placeholder='Write your text here...' />
+                </div>
+                <div className='btn-smrz'>
+                    <button>Generate text</button>
+                </div>
+                <div className='summarize-content-smrz'>
+                    <h1>Summarized Text</h1>
+                    <textarea value='' placeholder='Summarized Text' />
+                </div>
             </div>
         </div>
     );
