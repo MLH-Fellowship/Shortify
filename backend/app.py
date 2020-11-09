2538dc0847dd50ba25d3782e74688436c960f670
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-11-09 12:48:42 -0500

    Add Ability to Specify Ratio

--- backend/app.py
+++ backend/app.py
@@ -11,10 +11,11 @@ CORS(app)
 
 
 #### ROUTES
-@app.route('/<text>', methods=['POST'])
-def summarizeText(text):
+@app.route('/<string:text>/<float:ratio>', methods=['POST'])
+def summarizeText(text, ratio):
     model = Summarizer()
-    result = model(text, num_sentences=3)
+    #result = model(text, num_sentences=3)
+    result = model(text, ratio=ratio)
     return result
 
 if __name__ == "__main__":
