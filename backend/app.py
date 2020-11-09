3eb613200a00fbe68b15fb9214cbfbae96938c05
Aneesh Kodali <aneesh.kodali@gmail.com>
2020-11-09 09:41:44 -0500

    Connect Frontend and Backend

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,13 @@
 #### IMPORT 
 from flask import Flask
+from flask_cors import CORS 
+
 
 #### APP SETUP
 app = Flask(__name__)
+# enable resource sharing between frontend and backend
+CORS(app)
+
 
 #### ROUTES
 @app.route('/')
