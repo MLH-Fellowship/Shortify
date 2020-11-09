df1b6337fde8068c2e7a9f69bc839d61894225d9
msaaddev <mrsaadirfan@gmail.com>
2020-11-09 23:11:00 +0500

    👌 IMPROVE: dropdown styles

--- frontend/src/components/Summarize.js
+++ frontend/src/components/Summarize.js
@@ -1,61 +1,59 @@
 import React, { useState } from 'react';
-import '../styles/summarize.css';
 import backend from '../api/backend';
+import '../styles/summarize.css';
 
 const Summarize = () => {
-
+    const ratioOptions = [0.1, 0.2, 0.3, 0.4, 0.5];
     const [text, setText] = useState('');
-    //const sentenceOptions = [3,4,5,6];
-    const ratioOptions = [.1, .2, .3, .4, .5];
     const [ratioSelected, setRatioSelected] = useState(ratioOptions[0]);
     const [textSummarized, setTextSummarized] = useState('');
-    
+
     /**
      *
      * uses ML model to summarize content
      */
-
-
     const getSummarizeContent = async () => {
         const response = await backend.post(`/${text}/${ratioSelected}`);
         const { data } = response;
         setTextSummarized(data);
     };
 
+    /**
+     *
+     * rendering dropdown options on the screen
+     */
     const ratioOptionsRendered = ratioOptions.map(option => {
         return (
-            <option value={option}>{option}</option>
+            <option key={option} value={option}>
+                {option}
+            </option>
         );
     });
-
 
     return (
         <div className='container-smrz' id='summarize-area'>
             <div className='sub-container-smrz'>
                 <div className='main-content-smrz'>
                     <h1>Your Content</h1>
-                    <textarea 
+                    <textarea
                         placeholder='Write your text here...'
-                        onChange={(e) => setText(e.target.value)}
+                        onChange={e => setText(e.target.value)}
                     />
                 </div>
                 <div className='btn-smrz'>
-                    <button onClick={getSummarizeContent}>Generate text</button>
-                    <div className="dropdown-smrz">
+                    <div className='dropdown-smrz'>
                         <p>
-                            Ratio
-                            <select onChange={(e) => setRatioSelected(e.target.value)}>
+                            % of text
+                            <select onChange={e => setRatioSelected(e.target.value)}>
                                 {ratioOptionsRendered}
                             </select>
                         </p>
                     </div>
+                    <button onClick={getSummarizeContent}>Generate text</button>
                 </div>
                 <div className='summarize-content-smrz'>
                     <h1>Summarized Text</h1>
-                    <textarea 
-                        value={textSummarized} 
-                        placeholder='Summarized Text' 
-                    />
+                    <textarea value={textSummarized} placeholder='Summarized Text' />
                 </div>
             </div>
         </div>
